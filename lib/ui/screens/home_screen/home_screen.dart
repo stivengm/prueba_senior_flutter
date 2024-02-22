5a64ce7f998fb02127d4deeb5a75547f25d85ad3
Stiven Gomez <stiven.gomez@nuvu.cc>
2024-02-22 16:03:56 -0500

    FIX - Ajuste de vista con drawer.

--- lib/ui/screens/home_screen/home_screen.dart
+++ lib/ui/screens/home_screen/home_screen.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:prueba_senior_flutter_treebu/ui/drawers/home_drawer.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final scaffoldKey = GlobalKey<ScaffoldState>();
+    
+    return Scaffold(
+      appBar: AppBar(),
+      drawer: HomeDrawer(
+        scaffoldKey: scaffoldKey
+      ),
+      body: Placeholder(),
+    );
   }
 }
