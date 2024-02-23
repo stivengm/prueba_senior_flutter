095d00287ad69b2191a84079614556a66352a959
Stiven Gomez <stiven.gomez@nuvu.cc>
2024-02-22 23:01:06 -0500

    FIX - Ajuste de loginBloc en providersBloc.

--- lib/core/providers/bloc_provider.dart
+++ lib/core/providers/bloc_provider.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:prueba_senior_flutter_treebu/core/blocs/home/home_bloc.dart';
+import 'package:prueba_senior_flutter_treebu/core/blocs/login/login_bloc.dart';
 
 final List<BlocProvider> providerBloc = [
   BlocProvider<HomeBloc>(create: ( _ ) => HomeBloc()),
+  BlocProvider<LoginBloc>(create: ( _ ) => LoginBloc()),
 ];
